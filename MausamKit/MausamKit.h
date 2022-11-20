4028436dfda1b40a5bfbb8de77b647c476c59ace
Anurag Sharma <anurag.sharma.iosdev@gmail.com>
2022-11-20 22:36:26 +0530

    Created MausamKit Framework

---
+++ MausamKit/MausamKit.h
@@ -0,0 +1,18 @@
+//
+//  MausamKit.h
+//  MausamKit
+//
+//  Created by Anurag Sharma on 20/11/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MausamKit.
+FOUNDATION_EXPORT double MausamKitVersionNumber;
+
+//! Project version string for MausamKit.
+FOUNDATION_EXPORT const unsigned char MausamKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MausamKit/PublicHeader.h>
+
+
